2bea2085fe2e32b482bf448cc9bcd4fa52eb3997
Tej <tejtejas777k@gmail.com>
2023-06-11 18:20:17 +0530

    Added

---
+++ javascript/02-Fundamentals-Part-2/starter/ass2.js
@@ -0,0 +1,32 @@
+
+// 'use strict';
+
+// console.log(`hello`)
+
+// const calc = function(score1 , score2 , score3){
+//     return (score1+score2+score3)/3;
+// }
+
+// let DolphisAverageScore = calc(44,23,71);
+// let kolasaverageScare = calc(65,54,49);
+
+// console.log(`Average Score : ${DolphisAverageScore}, ${kolasaverageScare}`);
+
+// const winnerCheck = function(DolphisAverageScore,kolasaverageScare){
+//     if(DolphisAverageScore>(2*kolasaverageScare)){
+//         console.log(`Dolphin WINS (${DolphisAverageScore} vs ${kolasaverageScare})` );
+//     }else if(kolasaverageScare>(2*DolphisAverageScore)){
+//         console.log(`Kolos WINS (${DolphisAverageScore} vs ${kolasaverageScare})` );
+//     }else{
+//         console.log(`No one WINS!`);
+//     }
+// }
+
+// winnerCheck(DolphisAverageScore,kolasaverageScare);
+
+// //test 2 
+
+// DolphisAverageScore = calc(85,54,41);
+// kolasaverageScare = calc(23,27,34);
+// console.log(`Average Score : ${DolphisAverageScore}, ${kolasaverageScare}`);
+// winnerCheck(DolphisAverageScore,kolasaverageScare);
---
+++ javascript/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,113 @@
+// Strict MOde
+
+// 'use strict';
+
+// let hasdiverLicencese = false;
+// const passTest = true;
+
+// if(passTest) hasdiverLicencese =true;
+// if(hasdiverLicencese) console.log("I can Drive");
+
+// // const interface = 'Audio';
+// // const private = 345;
+
+// function logger(){
+//     console.log("My name Tej");
+// }
+// // invoking , calling ,runnning 
+// logger(); 
+// logger();
+// logger();
+
+// function fruitProcessor(apples , oranges){
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges .`;
+//     return juice; 
+// }
+
+// const appleJuice = fruitProcessor(5,20);
+// console.log(appleJuice);
+// console.log(fruitProcessor(2,6));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+
+
+//function declaration
+
+// function calcAge1(birthyear){
+//     return 2037 - birthyear;
+// }
+
+// console.log(calcAge1(1991));
+
+
+// //function expretion
+// const calcAge2 = function(birthAge){
+//     return 2037 - birthAge;
+// }
+// console.log(calcAge2(1991));
+
+// Arrow function
+
+// const calAge3 = birthyear => 2037 - birthyear;
+// console.log(calAge3(1991));
+
+// const yearsultilRetierment = (birthyear, firstname) => {
+//     const age = 2037 - birthyear;
+//     const retierment = 65 - age;
+//     return `${firstname} retiers in ${retierment} years`;
+// }
+
+// console.log(yearsultilRetierment(1991, 'Tej'));
+
+//functions
+
+
+// function cutfruitpieces(fruit){
+//     return fruit*4;
+// }
+
+// function fruitProcessor(apples , oranges){
+//     const applepieces = cutfruitpieces(apples);
+//     const orangepices = cutfruitpieces(oranges);
+
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${applepieces} apples peices and ${orangepices} oranges piceses .`;
+//     return juice; 
+// }
+
+// console.log(fruitProcessor(2,3));
+
+
+//
+
+
+// const calcAge = function(birthyear){
+//     return 2037 -birthyear;
+// }
+
+// const calcRetierment = function(age){
+//     return 65 -age;
+// } 
+
+// const yearsultilRetierment = function (birthyear, firstname){
+//     const age = calcAge(birthyear);
+//     const retierment = calcRetierment(age);
+// if(retierment>0){
+//     console.log(`${firstname} retiers in ${retierment} years`);
+//     return retierment;
+// }else{
+//     console.log(`${firstname} has already retiered `);
+//     return -1;
+// }
+// }
+
+// console.log(yearsultilRetierment(1991, 'Tej'));
+// console.log(yearsultilRetierment(1950, 'Tej'));
+
+
+ 
+
+
