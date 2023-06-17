6d9f1402bc10010d0ca754c2ae142b55dac32093
Tej <tejtejas777k@gmail.com>
2023-06-17 18:42:22 +0530

    updated

--- javascript/02-Fundamentals-Part-2/starter/ass2.js
+++ javascript/02-Fundamentals-Part-2/starter/ass2.js
@@ -1,5 +1,5 @@
 
-// 'use strict';
+'use strict';
 
 // console.log(`hello`)
 
@@ -30,3 +30,58 @@
 // kolasaverageScare = calc(23,27,34);
 // console.log(`Average Score : ${DolphisAverageScore}, ${kolasaverageScare}`);
 // winnerCheck(DolphisAverageScore,kolasaverageScare);
+
+// coding challeng 2 in arrays 
+
+// const tipcalculator =  function(bill){
+//     if(bill>50 && bill<300){
+//         return 0.15*bill;
+//     }else{
+//         return 0.20*bill;
+//     }
+// }
+
+// const bill = [125 , 555 , 44];
+// console.log(`Bill Array : ${bill}`);
+// const tips = [tipcalculator(bill[0]) , tipcalculator(bill[1]) , tipcalculator(bill[2])];
+//  console.log(`Tips Array : ${tips}`); 
+
+// const total  = [bill[0]+tips[0] + bill[1]+tips[1] + bill[2]+tips[2]];
+// console.log(`Total : ${total}`);
+
+/// challenge 3
+
+// const Mark = {
+//     fullname:'Mark Miller',
+//     mass:78,
+//     height:1.69,
+
+//     calcBMI:function(){
+//         this.bmi =  this.mass/(this.height**2);
+//         return this.bmi;
+//     }
+// }
+ 
+// const Jhon = {
+//     fullname:'Jhon Smith',
+//     mass:92,
+//     height:1.95,
+
+//     calcBMI:function(){
+//         this.bmi = this.mass/(this.height**2);
+//         return this.bmi;
+//     }
+// }
+  
+
+// const summary=function(){
+//     if(Mark.calcBMI() > Jhon.calcBMI()){
+//         return  `${Mark.fullname}'s BMI ${Mark.calcBMI()} is higher than ${Jhon.fullname}'s ${Jhon.calcBMI()}!`;
+//     }else{
+//         return  `${Jhon.fullname}'s BMI ${Jhon.calcBMI()} is higher than ${Mark.fullname}'s ${Mark.calcBMI()}!`;
+
+//     }
+// }
+
+// console.log(summary());
+// console.log(Mark.bmi , Jhon.bmi);
--- javascript/02-Fundamentals-Part-2/starter/script.js
+++ javascript/02-Fundamentals-Part-2/starter/script.js
@@ -108,6 +108,239 @@
 // console.log(yearsultilRetierment(1950, 'Tej'));
 
 
+ //Arrays 
+
+//  const friend1 =  'Shashank';
+//  const friend2 = ' Stevan';
+//  const friend3 = 'peter'; 
+
+// const friends = ['Shashank', 'Steven' , 'Peter'];
+// console.log(friends);
+
+// const years = new Array(1991 ,1984 ,2004 , 2000);
+
+// console.log(friends[0]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length -1 ]);
  
+// friends[2] = 'Jay';
+// console.log(friends);  //changed 
+
+// const firstname = 'tej';
+// const tej = [firstname, 'shivappa' , 2037 -1991 , 'student' , friends ]; // can also insert an another array
+// console.log(tej);
+// console.log(tej.length);
+
+// //Exercise 
+
+// const calcAge = function(birthyear){
+//     return 2037 - birthyear;
+// }
+
+// const yearss  = [1991,1967 ,2002 ,2010,2018];
+
+// const age1 = calcAge(yearss[0]);
+// const age2 = calcAge(yearss[1]);
+// const age3 = calcAge(yearss[yearss.length -1]);
+
+// console.log( age1 , age2 , age3);
+
+//  const ages = [calcAge(yearss[0]),calcAge(yearss[1]),calcAge(yearss[yearss.length -1])];
+//  console.log(ages);
+
+//Array operations
+
+// const friends = ['Shashank', 'Steven' , 'Peter'];
+
+//add elements 
+// friends.push('jay'); // push => meathod
+// console.log(friends);
+
+// friends.unshift('yashu'); //from the first
+// console.log(friends);
+
+// //remove elements
+
+// const popped1 = friends.pop();
+// console.log(popped1);
+// console.log(friends);
+// const popped2 = friends.pop();
+// console.log(popped2);
+// console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+
+// console.log(friends.indexOf('Steven'));
+
+// friends.push(23);
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('bob'));
+// console.log(friends.includes('23')); //fales => does't do type coertion 
+// console.log(friends.includes(23)); //trure
+
+// if(friends.includes('Steven')){
+//     console.log('You have a friend called Steven');
+// }
 
 
+
+//Objects
+
+
+// const tej = [
+//     'Jhonas',
+//     'Yashu',
+//     2037-1991,
+//     'Student',
+//     ["micle", "peter", "Steven"]
+
+// ];
+
+// const tejArray = { //{}  => is define new objects
+//     firstname: 'Tej',
+//     lastname: 'Shivappa',
+//     job:'STudent', 
+//     age:2037-1991,
+//     friends :['Michel','Peter','Steven']    
+// };
+
+
+// Dot vs Bracket Notation
+
+// const tejArray = { //{}  => is define new objects
+//     firstname: 'Tej',
+//     lastname: 'Shivappa',
+//     job:'STudent', 
+//     age:2037-1991,
+//     friends :['Michel','Peter','Steven']    
+// };
+
+
+// console.log(tejArray.lastname);
+// console.log(tejArray['lastname']);
+
+// const namekey = 'name';
+// console.log(tejArray['first'+namekey]);  //concatenate
+// console.log(tejArray['last'+namekey]);  //[ inside is an expresion]
+
+
+// const intrestedin = prompt('What do you want to know about tej ? Choose between firstname , lastname , age , job , and friends');
+// console.log(tejArray[intrestedin]);
+
+// if(tejArray[intrestedin]){
+//     console.log(tejArray[intrestedin]);
+// }else{
+//     console.log('Wrong choice ');
+// }
+
+// tejArray.location = 'India';
+// tejArray['instagram'] = '@t_e_j_7777777_';
+// console.log(tejArray);
+
+//challenge 
+// console.log(`${tejArray.firstname} has ${tejArray.friends.length} friends , and his best friend is called ${tejArray.friends[0]}`);
+
+
+// Object Meathod 
+
+// const tej = { //{}  => is define new objects
+//     firstname: 'Tej',
+//     lastname: 'Shivappa',
+//     job:'STudent', 
+//     birthyear:2002,
+//     friends :['Michel','Peter','Steven']  ,
+    // hasDriverlicense : true  ,
+
+//    calcAge: function(birthyear){ //this is an expression 
+//     return 2037 - birthyear;
+//    }
+
+
+// calcAge: function(){ //this is an expression 
+//     return 2037 - this.birthyear; // so, this is reffered to tej
+//    }
+
+
+//  calcAge:function(){
+//     this.age = 2037 - this.birthyear;  
+//    return this.age;
+
+//   },
+//    hasDriverlicense: function(){
+//          if(this.age>=18){
+//             return `a` ;
+//          }else{
+//             return `no`; //it will be returned in ``;
+//          }
+//    }
+
+
+// };
+// console.log(tej.calcAge()); //tej is calling the function
+// console.log(tej['calcAge'](1991));
+
+// console.log(tej.age);
+// console.log(tej.age);
+// console.log(tej.age);
+
+// console.log(` ${tej.firstname} is a ${tej.calcAge()} old Student , and he has ${tej.hasDriverlicense()} : drive's license`);
+
+
+//Loops  
+
+//   console.log('Lifting WEIGHTS repetition 1 ');
+
+//   for(let rep =1 ; rep<= 10 ; rep++){
+//     console.log(`Lifting WEIGHTS repetition ${rep}`);
+
+//   }
+
+  const tej = [
+    'Jhonas',
+    'Yashu',
+    2037-1991,
+    'Student',
+    ["micle", "peter", "Steven"]
+
+];
+
+// const types=[];
+
+// for(let i=0 ; i<=tej.length-1;i++){
+//     console.log(tej[i] , typeof tej[i] );
+ 
+//     //filling an array 
+//     // types[i]=typeof tej[i];
+
+//     types.push(typeof tej[i]);
+
+// }
+// console.log(types);
+
+const years = [1991,2007,1969,2020];
+const ages = [];
+
+// for(let i =0 ; i<years.length;i++){
+//     ages.push(2037-years[i]);
+// }
+
+// console.log(ages);
+
+//Continue and Break Statements
+
+for(let i=0 ; i<=tej.length-1;i++){
+
+     if( typeof tej[i] !== 'string' ) continue;
+    console.log(tej[i] , typeof tej[i] );
+
+
+}
+for(let i=0 ; i<=tej.length-1;i++){
+
+    if( typeof tej[i] === 'number' ) break;
+   console.log(tej[i] , typeof tej[i] );
+
+
+}
